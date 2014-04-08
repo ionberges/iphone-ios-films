c2a5fb07f46e63b370e16e949ccf2e7e1b791e60
ion berges <ionberges@gmail.com>
2014-04-08 13:18:35 +0100

    Update cartelera.h

--- cine/cartelera.h
+++ cine/cartelera.h
@@ -1,10 +1,3 @@
-//
-//  cartelera.h
-//  cine
-//
-//  Created by iMac 6 on 20/02/13.
-//  Copyright (c) 2013 iMac 6. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 #import "pelicula.h"
@@ -14,11 +7,9 @@
 + (cartelera *)sharedInstance;
 @property (strong,nonatomic) NSMutableArray *peliculas;
 
-//@property (strong,nonatomic) NSURL *cache;
+
 @property (strong,nonatomic) NSURL *cacheMini;
 @property (strong,nonatomic) NSArray *listado;
 
-//typedef void(^consulta500pxCompletion)(void);
-//-(void)consulta500px:(NSString *)feature completion:(consulta500pxCompletion)completion;
 
 @end
